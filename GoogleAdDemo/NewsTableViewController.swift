1f6e7a003e33952fa14ba38dc6f8c5bf6564fba3
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-08-02 10:14:02 +0300

    Add SlideUp animation to adBanner

--- GoogleAdDemo/NewsTableViewController.swift
+++ GoogleAdDemo/NewsTableViewController.swift
@@ -90,8 +90,16 @@ class NewsTableViewController: UITableViewController {
 extension NewsTableViewController: GADBannerViewDelegate {
     func adViewDidReceiveAd(_ bannerView: GADBannerView) {
         print("Banner loaded successully")
-        tableView.tableHeaderView?.frame = bannerView.frame
-        tableView.tableHeaderView = bannerView
+        
+        //Reposition the banner ad to create a slide down effect
+        let translateTransform = CGAffineTransform(translationX: 0, y: bannerView.bounds.size.height)
+        bannerView.transform = translateTransform
+        
+        UIView.animate(withDuration: 0.5) {
+            self.tableView.tableHeaderView?.frame = bannerView.frame
+            bannerView.transform = CGAffineTransform.identity
+            self.tableView.tableHeaderView = bannerView
+        }
     }
     
     func adView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: GADRequestError) {
